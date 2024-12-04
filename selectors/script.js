0c64265820c6f53a46f2b206e08363e2d152c058
Harshv1209 <1320204019@bitraipur.ac.in>
2024-12-04 22:27:05 +0530

    selectors

---
+++ selectors/script.js
@@ -0,0 +1,20 @@
+//get element by TagName
+// var a= document.getElementsByTagName("li");
+// console.log(a)
+
+//getElementById
+// var a= document.getElementById("hero");
+// console.log(a)
+
+//getElementsByClassName
+var a = document.getElementsByClassName("hero");
+console.log(a);
+
+//querySelector
+var a = document.querySelector(".hero");
+console.log(a);
+
+//querySelectorAll
+
+var a = document.querySelectorAll("li");
+console.log(a);
