ca178fd5da0694ec1ed56efdc7cb90872a2ba5fc
Harshv1209 <1320204019@bitraipur.ac.in>
2024-12-27 20:34:35 +0530

    styling

--- selectors/script.js
+++ selectors/script.js
@@ -1,6 +1,2 @@
-//styling
-let a = document.querySelector("h1");
-a.style.color = "orange";
-a.style.backgroundColor = "blue";
-a.style.border = "2px solid red";
-a.style.borderRadius = "10px";
+let hero = document.querySelector("#hero");
+console.log(hero.getAttribute('about'));
