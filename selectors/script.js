53d9168735961930e521fb959d03effe13d0acd8
Harshv1209 <1320204019@bitraipur.ac.in>
2024-12-27 20:06:04 +0530

    styling

--- selectors/script.js
+++ selectors/script.js
@@ -1,32 +1,6 @@
-// traverse the DOM
-
-//Par element
-// var parent = document.querySelector('ul');
-// var p = parent.parentElement;
-// console.log(p);
-
-//previous element siblings
-// console.log(document.querySelector(".hero").previousElementSibling);
-
-//children
-// var child = document.querySelector('ul');
-
-// console.log(child.children);
-// console.log(child.childNodes);
-
-//append child
-// const ul = document.querySelector("ul");
-// const newE = document.createElement("li");
-// newE.textContent = "Vaidya";
-// ul.appendChild(newE);
-
-//remove child
-// const ul = document.querySelector("ul");
-// ul.remove();
-
-// replace child
-const list = document.querySelector("ul");
-const childToChange = list.children[4];
-const newLi = document.createElement("li");
-newLi.textContent = "Harsh Beniwal";
-list.replaceChild(newLi, childToChange);
+//styling
+let a = document.querySelector("h1");
+a.style.color = "orange";
+a.style.backgroundColor = "blue";
+a.style.border = "2px solid red";
+a.style.borderRadius = "10px";
